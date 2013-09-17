0072ea44327cacb6809c0c4e5ea84eb46d67cee5
Toby Jaffey <toby@1248.io>
2013-09-18 00:35:59 +0100

    Cleanups. First plugtest case

---
+++ test/common.js
@@ -0,0 +1,8 @@
+exports.udpBearer = require('../lib/udp.js');
+exports.server = require('../lib/server');
+exports.client = require('../lib/client');
+exports.TEST_URL_BASE = 'coap://127.0.0.1:5683';
+exports.TEST_ENDPOINT = '/test';
+
+
+
