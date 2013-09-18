dc30c1570d553b8cb63531709a6350c7c5573ae0
Toby Jaffey <toby@1248.io>
2013-09-18 17:27:06 +0100

    Beginnings of test 14, lossy network.

---
+++ test/TD_COAP_CORE_14.js
@@ -0,0 +1,77 @@
+console.log('TD_COAP_CORE_01 Perform GET transaction (CON mode)');
+
+var common = require('./common.js');
+erbium = require('node-erbium');
+udpApp = common.udpBearer();
+coapServerApp = common.server();
+coapClientApp = common.client();
+
+var count = 2;
+
+function check1(raw) {
+if (count > 0) {
+    count--;
+    return false;
+}
+/*
+    common.checkStep(2);
+    var pkt = new erbium.Erbium(raw);
+    if (pkt.getHeaderType() != 0)
+        throw new Error('Wrong type');
+    if (pkt.getHeaderStatusCode() != 1)
+        throw new Error('Wrong code');
+*/
+}
+
+function check2(raw) {
+/*
+    common.checkStep(3);
+    var pkt = new erbium.Erbium(raw);
+    if (pkt.getHeaderStatusCode() != 69)
+        throw new Error('Wrong code');
+    if (pkt.getHeaderContentType() != erbium.TEXT_PLAIN)
+        throw new Error('Wrong type');
+    if (pkt.getHeaderMID() != 0x1234)
+        throw new Error('Wrong MID '+pkt.getHeaderMID());
+*/
+}
+
+coapServerApp.get(common.TEST_ENDPOINT, function(req, res) {
+    res.setContentType('text/plain');
+    res.send(erbium.CONTENT_2_05, 'Hello world');
+});
+
+function stimulus1() {
+/*
+    common.checkStep(1);
+*/
+    coapClientApp.get(erbium.COAP_TYPE_CON, common.TEST_URL_BASE + common.TEST_ENDPOINT, {
+        mid: 0x1234,
+        beforeSend: check1,
+        beforeReceive: check2,
+        success: function(inpkt, payload) {
+/*
+            common.checkStep(4);
+*/
+            console.log(payload.toString());
+            process.exit(0);
+        },
+        error: function() {
+            console.log("Error");
+        },
+        timeout: function() {
+            console.log("Timeout");
+        }
+    });
+}
+
+udpApp.start(5683, coapClientApp, coapServerApp, function(err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    coapServerApp.start();
+    stimulus1();
+});
+
+
