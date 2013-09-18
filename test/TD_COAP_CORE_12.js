e8b6f5111e0195fba12cca6183cd6e171f53c8d7
Toby Jaffey <toby@1248.io>
2013-09-18 14:40:20 +0100

    Multi-segment path

---
+++ test/TD_COAP_CORE_12.js
@@ -0,0 +1,59 @@
+console.log('TD_COAP_CORE_12 Handle request containing several URI-Path options');
+
+var common = require('./common.js');
+erbium = require('node-erbium');
+udpApp = common.udpBearer();
+coapServerApp = common.server();
+coapClientApp = common.client();
+
+exports.TEST_ENDPOINT = '/seg1/seg2/seg3';
+
+function check1(raw) {
+    common.checkStep(2);
+    var pkt = new erbium.Erbium(raw);
+    if (pkt.getHeaderType() != 0)
+        throw new Error('Wrong type');
+    if (pkt.getHeaderStatusCode() != 1)
+        throw new Error('Wrong code');
+}
+
+function check2(raw) {
+    common.checkStep(3);
+    var pkt = new erbium.Erbium(raw);
+    if (pkt.getHeaderStatusCode() != 69)
+        throw new Error('Wrong code');
+    if (pkt.getHeaderContentType() != erbium.TEXT_PLAIN)
+        throw new Error('Wrong type');
+    if (pkt.getHeaderMID() != 0x1234)
+        throw new Error('Wrong MID '+pkt.getHeaderMID());
+}
+
+coapServerApp.get(common.TEST_ENDPOINT, function(req, res) {
+    res.setContentType('text/plain');
+    res.send(erbium.CONTENT_2_05, 'Hello world');
+});
+
+function stimulus1() {
+    common.checkStep(1);
+    coapClientApp.get(erbium.COAP_TYPE_CON, common.TEST_URL_BASE + common.TEST_ENDPOINT, {
+        mid: 0x1234,
+        beforeSend: check1,
+        beforeReceive: check2,
+        success: function(inpkt, payload) {
+            common.checkStep(4);
+            console.log(payload.toString());
+            process.exit(0);
+        }
+    });
+}
+
+udpApp.start(5683, coapClientApp, coapServerApp, function(err) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    coapServerApp.start();
+    stimulus1();
+});
+
+
